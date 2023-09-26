14778d8fbc3420ded791b3804ada1d517d2cffcf
savhsergio <sergioandresherrera@gmail.com>
2023-09-25 22:27:49 -0500

    added coded for basic email with basic styles

--- Fun_with_E-mails.py
+++ Fun_with_E-mails.py
@@ -10,9 +10,52 @@ Automating customer support flows with e-mail responses
 """
 import smtplib
 import config
-server=smtplib.SMTP('smtp.gmail.com',587)
+#sending email
+server=smtplib.SMTP('smtp.gmail.com',587)#gmail always run on port 587
 server.starttls()
 server.login(config.fromaddr,config.password)
 msg='Some Nice Msg'
 server.sendmail(config.fromaddr,config.toaddr,msg)
 server.quit()
+#email encription
+try:
+    server.set_debuglevel(True)
+    print('sending ehlo')
+    server.ehlo()
+    if server.has_extn('STARTTLS'):
+        print('starting TLS session')
+        server.starttls()
+        print('Sending ehlo again')
+        server.ehlo
+finally:
+    server.quit()
+
+#Beautifying e-mail messages with MIME
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import email.utils
+
+#construct email message with the MIme Module
+
+"""For those who are not aware, a MIME multipart message means both HTML and text content in a single e-mail. 
+So, in this code, we create a new multipart MIME message and then add the text content to it."""
+fromaddr=config.fromaddr
+toaddr=config.toaddr
+msg=MIMEMultipart()
+msg['Subject']='Hello from the author of this file'
+msg['To']=email.utils.formataddr(('Recipient',toaddr))
+msg['From']=email.utils.formataddr(('Author',fromaddr))
+msgBody=MIMEText(body,'plain')
+msg.attach(msgBody)
+
+server=smtplib.SMTPT('smtp.gmail.com',587)
+server.starttls()
+server.login(fromaddr,config.password)
+text=msg.as_string()
+print('Text is :',text)
+server.sendmail(fromaddr,toaddr,text)
+server.quit
+
+
+#Email messags with attachments
+
