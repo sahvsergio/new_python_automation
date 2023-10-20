c1b62e9e18faa238a5008828c377d43115b45be1
savhsergio <sergioandresherrera@gmail.com>
2023-10-19 22:51:59 -0500

    started chapter on working with presentations

---
+++ Presentations_and_more.py
@@ -0,0 +1,7 @@
+#Reading PowerPoint presentations
+#Creating and updating presentations, and adding slides
+#Playing with layouts, placeholders, and textboxes
+#Working with different shapes and adding tables
+#Visual treat with pictures and charts
+#Automating weekly sales reports
+
