a7b75c3c7f1de4b63b8975f5a104696711350457
savhsergio <sergioandresherrera@gmail.com>
2023-10-27 21:51:19 -0500

    Adding A two content slide

--- Presentations_and_more.py
+++ Presentations_and_more.py
@@ -40,7 +40,53 @@ print('The text is:',text_runs)
 
 
 # Creating and updating presentations, and adding slides
+from pptx import Presentation
+prs=Presentation()
+slide=prs.slides.add_slide(prs.slide_layouts[0])
+slide.shapes.title.text='Yo, Python'
+slide.placeholders[1].text='Yes, it is really awesome'
+prs.save('yoPython.pptx')
+
+"""We can also create a new presentation from an existing presentation. 
+In the next code example, we take a PowerPoint template, create a new PPT, 
+and add a slide to it with text content. We use the following template for this example:"""
+
+from pptx import Presentation 
+prs = Presentation('yoPython.pptx') 
+ 
+first_slide = prs.slides[0] 
+first_slide.shapes[0].text_frame.paragraphs[0].text = "Hello!" 
+ 
+slide = prs.slides.add_slide(prs.slide_layouts[1]) 
+text_frame = slide.shapes[0].text_frame 
+p = text_frame.paragraphs[0] 
+p.text = "This is a paragraph" 
+ 
+prs.save('new_ppt.pptx')
 # Playing with layouts, placeholders, and textboxes
+
+from pptx import Presentation
+prs=Presentation()
+two_content_slide_layout=prs.slide_layouts[3]
+slide=prs.slides.add_slide(two_content_slide_layout)
+shapes=slide.shapes
+title_shape=shapes.title
+title_shape.text='Adding A two content slide'
+
+
+body_shape=shapes.placeholders[1]
+tf=body_shape.text_frame    
+tf.text='This is line 1'
+
+p=tf.add_paragraph()
+p.text='Again a line 2'
+p.level=1
+
+p=tf.add_paragraph()
+p.text='And this is line 3 ...'
+p.level=2
+
+prs.save('two_content.pptx')
 # Working with different shapes and adding tables
 # Visual treat with pictures and charts
 # Automating weekly sales reports
